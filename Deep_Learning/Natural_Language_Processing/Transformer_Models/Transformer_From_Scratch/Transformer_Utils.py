21e61d9287ba343885101f0a2a6e926bd93d316e
suresh-venkate <76463566+suresh-venkate@users.noreply.github.com>
2021-08-04 20:10:17 +0530

    Added function clones

--- Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/Transformer_Utils.py
+++ Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/Transformer_Utils.py
@@ -63,23 +63,15 @@ class AddAndNorm(nn.Module):
   def forward(self, x, sublayer):
     return self.norm(x + sublayer(x))
 
-
-
-
-
-
-
-
-
-    def clones(module, N):
-    """
-    Produce N identical layers of module
-    Arguments:
-      module: module to be cloned
-      N: Number of time the module will be cloned
-    Returns:
-      Cloned module
-    """
-    cloned_module = nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
-    return cloned_module
-
+### Function: clones
+def clones(module, N):
+"""
+Produce N identical layers of module
+Arguments:
+  module: module to be cloned
+  N: Number of time the module will be cloned
+Returns:
+  Cloned module
+"""
+cloned_module = nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+return cloned_module

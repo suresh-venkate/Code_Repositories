f2a065ad34b13c69e86938087f4555d00560dd3c
suresh-venkate <76463566+suresh-venkate@users.noreply.github.com>
2021-08-05 07:03:33 +0530

    transformer_utils.py updated

--- Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/transformer_utils.py
+++ Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/transformer_utils.py
@@ -8,6 +8,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+
+### Function: clones
+def clones(module, N):
+  """
+  Produce N identical layers of module
+  Arguments:
+    module: module to be cloned
+    N: Number of time the module will be cloned
+  Returns: Cloned module
+  """
+  cloned_module = nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+  return cloned_module
+
 ### Class: Embeddings
 class Embeddings(nn.Module):
   """
@@ -50,6 +63,123 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
       x = x + self.pe[:, :x.size(1)].clone().detach()
       return self.dropout(x)
+    
+### Class: ScaledDotProductAttention
+class ScaledDotProductAttention(nn.Module):
+  """
+  Compute Scaled Dot Product Attention (Ref: Section 3.2.1, Figure 2 (left))
+  """
+
+  def __init__(self, scaling):
+    """
+    Arguments:
+      scaling: scaling to use while computing attention
+    """
+    super(ScaledDotProductAttention, self).__init__()
+    self.scaling = scaling
+    self.softmax = nn.Softmax(dim = -1)
+  
+  def forward(self, Q, K, V, attn_mask = None):
+    """
+    Arguments:
+      Q: Query tensor
+      K: Key tensor
+      V: Value tensor
+      attn_mask: Optional mask to mask out some query-key combinations
+    Returns:
+      probs: softmax(Q * K.T / scaling)
+      attn: probs.V
+    """
+    scores = torch.matmul(Q, K.transpose(-2, -1)) # Matmul of Q and K
+    scores = scores / self.scaling # Apply Scaling to maintain original variance
+    if attn_mask is not None: # Apply mask (optional)
+      scores = scores.masked_fill(attn_mask == 0, -1e9)
+    probs = self.softmax(scores) # Compute softmax
+    attn = torch.matmul(probs, V)
+
+    return attn, probs
+
+### Class: MultiHeadAttention
+class MultiHeadAttention(nn.Module):
+  """
+  Compute Multi-Head Attention (Ref: Section 3.2.2, Figure 2 (right))
+  """
+  def __init__(self, h, d_model, attn_dropout = 0.1):
+    """
+    Arguments:
+      h: Number of parallel attention layers (heads)
+      d_model: Size of input embeddings
+      attn_dropout: dropout value to use in MHA module
+    """
+    super(MultiHeadAttention, self).__init__()
+    assert d_model % h == 0 # Confirm that input embedding size is a multiple of # heads
+    self.d_k = d_model // h # Dimension of projected outputs
+    self.h = h # Number of heads
+    self.attn = None # Placeholder to store attention softmax output
+
+    # Define linear layers for projecting Q, K, V
+    self.wi_q = nn.Linear(d_model, d_model, bias = False)
+    self.wi_k = nn.Linear(d_model, d_model, bias = False)
+    self.wi_v = nn.Linear(d_model, d_model, bias = False)
+
+    # Define SDPA layer
+    self.attention = ScaledDotProductAttention(scaling = self.d_k ** 0.5)
+
+    # Define final FC and dropout layers
+    self.fc = nn.Linear(d_model, d_model, bias = False)
+    self.dropout = nn.Dropout(p = attn_dropout)
+        
+  def forward(self, Q, K, V, mask = None):
+    if mask is not None:
+      mask = mask.unsqueeze(1) # Head axis broadcasting - Same mask applied to all h heads.
+    
+    nb = Q.size(0) # Extract number of batches in Q 
+    # Q, K, V are of shape nb x lq x d_model
+    # Pass through linear layers and separate each head to get
+    # outputs of shape nb x lq x h x d_k
+    Q = self.wi_q(Q).view(nb, -1, self.h, self.d_k)
+    K = self.wi_k(K).view(nb, -1, self.h, self.d_k)
+    V = self.wi_v(V).view(nb, -1, self.h, self.d_k)
+
+    # Transpose lq and h for attention dot product
+    # Shape is now nb x h x lq x d_k
+    Q, K, V = Q.transpose(1, 2), K.transpose(1, 2), V.transpose(1, 2)
+        
+    # Apply Scaled Dot Product Attention
+    # Shape of x is nb x h x lq x d_k
+    x, self.attn = self.attention(Q, K, V, attn_mask = mask)
+
+    # Transpose nb and h dimensions and concatenate all heads together into 
+    # a single unit of dimension h x d_k = d_model 
+    x = x.transpose(1, 2).contiguous().view(nb, -1, self.h * self.d_k)
+
+    # Apply final linear layer and dropout
+    x = self.dropout(self.fc(x))
+    
+    return x
+
+### Class: PositionwiseFeedForward
+class PositionwiseFeedForward(nn.Module):
+  """
+  Implements the position-wise feed-forward layer (Ref: Section 3.3, Eqn. (2))
+  """
+  def __init__(self, d_model, d_ff, pwff_dropout = 0.1):
+    """
+    Arguments: 
+      d_model: Dimensionality of input and output of PWFFN layer
+      d_ff: Dimension of hidden layer
+      pwff_dropout: Dropout value to use for position wise feedforward layers
+    """
+    super(PositionwiseFeedForward, self).__init__()
+    self.fc_1 = nn.Linear(d_model, d_ff) # First transformation
+    self.fc_2 = nn.Linear(d_ff, d_model) # Second transformation
+    self.relu = nn.ReLU()  
+    self.dropout = nn.Dropout(pwff_dropout)
+
+  def forward(self, x):
+    x = self.relu(self.fc_1(x))
+    x = self.dropout(self.fc_2(x))
+    return x
 
 ### Class: AddAndNorm    
 class AddAndNorm(nn.Module):
@@ -63,14 +193,56 @@ class AddAndNorm(nn.Module):
   def forward(self, x, sublayer):
     return self.norm(x + sublayer(x))
 
-### Function: clones
-def clones(module, N):
+### Class: EncoderLayer 
+class EncoderLayer(nn.Module):
   """
-  Produce N identical layers of module
-  Arguments:
-    module: module to be cloned
-    N: Number of time the module will be cloned
-  Returns: Cloned module
+  Single Encoder Unit comprising of a multi-head-attention unit with add_and_norm followed by
+  a position-wise-feed-forward-network with add_and_norm (Ref: Section 3.1, Encoder)
   """
-  cloned_module = nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
-  return cloned_module
+  def __init__(self, d_model, h, attn_dropout, d_ff, pwff_dropout):
+    """
+    Arguments:
+      d_model: Size of input embeddings    
+      h: Number of parallel attention layers (heads)
+      attn_dropout: dropout value to use in MHA module
+      d_ff: Dimension of hidden layer
+      pwff_dropout: Dropout value to use for position wise feedforward layers
+    """
+    super(EncoderLayer, self).__init__()
+    self.MHA_unit = MultiHeadAttention(h, d_model, attn_dropout)
+    self.PWFFN = PositionwiseFeedForward(d_model, d_ff, pwff_dropout)
+    self.addandnorm_MHA = AddAndNorm(d_model)
+    self.addandnorm_PWFFN = AddAndNorm(d_model)
+
+  def forward(self, x, mask):
+    x = self.addandnorm_MHA(x, lambda x: self.MHA_unit(x, x, x, mask))
+    x = self.addandnorm_PWFFN(x, self.PWFFN)
+    return x
+
+### Class: Encoder
+class Encoder(nn.Module):
+  """
+  Encoder is a stack of N EncoderLayers
+  """
+  def __init__(self, d_model, h, attn_dropout, d_ff, pwff_dropout, N):
+    """
+    Arguments:
+      d_model: Size of input embeddings    
+      h: Number of parallel attention layers (heads)
+      attn_dropout: dropout value to use in MHA module
+      d_ff: Dimension of hidden layer
+      pwff_dropout: Dropout value to use for position wise feedforward layers    
+      N: Number of EncoderLayers in the Encoder stack
+    """
+    super(Encoder, self).__init__()
+    self.enclayer = EncoderLayer(d_model, h, attn_dropout, d_ff, pwff_dropout)
+    self.enclayer_stack = clones(self.enclayer, N)
+    self.norm = nn.LayerNorm(d_model, eps = 1e-6)
+        
+  def forward(self, x, mask):
+    x = self.norm(x)
+    for layer in self.enclayer_stack:
+      x = layer(x, mask)
+    return x
+
+

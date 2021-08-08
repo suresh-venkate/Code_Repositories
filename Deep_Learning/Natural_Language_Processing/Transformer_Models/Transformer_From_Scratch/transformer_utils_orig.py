e1269df9ae6a9d4f5b8741fff3871bc7ee9f1a04
suresh-venkate <76463566+suresh-venkate@users.noreply.github.com>
2021-08-08 09:15:44 +0530

    utils_orig.py updated

--- Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/transformer_utils_orig.py
+++ Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/transformer_utils_orig.py
@@ -34,7 +34,7 @@ class EncoderLayer(nn.Module):
         super(EncoderLayer, self).__init__()
         self.self_attn = self_attn
         self.feed_forward = feed_forward
-        self.sublayer = clones(AddAndNorm(size), 2)
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
         self.size = size
 
     def forward(self, x, mask):

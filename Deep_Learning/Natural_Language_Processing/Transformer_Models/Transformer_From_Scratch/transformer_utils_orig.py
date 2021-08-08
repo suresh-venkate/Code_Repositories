a8a75e50443531333b8f5f8c88f8769516a7dc8c
suresh-venkate <76463566+suresh-venkate@users.noreply.github.com>
2021-08-08 07:31:30 +0530

    utils_orig.py updated

--- Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/transformer_utils_orig.py
+++ Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/transformer_utils_orig.py
@@ -66,21 +66,6 @@ class EncoderLayer(nn.Module):
     x = self.addandnorm_PWFFN(x, self.PWFFN)
     return x
 
-# class EncoderLayer(nn.Module):
-    # "Encoder is made up of self-attn and feed forward (defined below)"
-    # def __init__(self, size, self_attn, feed_forward, dropout):
-        # super(EncoderLayer, self).__init__()
-        # self.self_attn = self_attn
-        # self.feed_forward = feed_forward
-        # #self.sublayer = clones(SublayerConnection(size, dropout), 2)
-        # self.sublayer = clones(AddAndNorm(size), 2)
-        # self.size = size
-
-    # def forward(self, x, mask):
-        # "Follow Figure 1 (left) for connections."
-        # x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
-        # return self.sublayer[1](x, self.feed_forward)
-
 ### Class: Encoder
 class Encoder(nn.Module):
   """
@@ -102,23 +87,10 @@ class Encoder(nn.Module):
     self.norm = nn.LayerNorm(d_model, eps = 1e-6)
         
   def forward(self, x, mask):
-    #x = self.norm(x)
+    x = self.norm(x)
     for layer in self.enclayer_stack:
       x = layer(x, mask)
     return self.norm(x)
-
-# class Encoder(nn.Module):
-    # "Core encoder is a stack of N layers"
-    # def __init__(self, layer, N):
-        # super(Encoder, self).__init__()
-        # self.layers = clones(layer, N)
-        # self.norm = nn.LayerNorm(layer.size, eps = 1e-6)
-        
-    # def forward(self, x, mask):
-        # "Pass the input (and mask) through each layer in turn."
-        # for layer in self.layers:
-            # x = layer(x, mask)
-        # return self.norm(x)
 
 class DecoderLayer(nn.Module):
     "Decoder is made of self-attn, src-attn, and feed forward (defined below)"
@@ -180,13 +152,6 @@ def make_model(src_vocab, tgt_vocab, N=6,
     attn = MultiHeadAttention(h, d_model, attn_dropout = dropout)
     ff = PositionwiseFeedForward(d_model, d_ff, dropout)
     position = PositionalEncoding(d_model, dropout)
-    # model = EncoderDecoder(
-        # Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
-        # Decoder(DecoderLayer(d_model, c(attn), c(attn), 
-                             # c(ff), dropout), N),
-        # nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
-        # nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
-        # Generator(d_model, tgt_vocab))
     model = EncoderDecoder(
         Encoder(d_model, h, dropout, d_ff, dropout, N),
         Decoder(DecoderLayer(d_model, c(attn), c(attn), 

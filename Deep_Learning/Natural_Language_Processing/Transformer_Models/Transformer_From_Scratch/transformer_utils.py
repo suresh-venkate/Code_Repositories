2a905386f81671b3f8fe91a53572ec6866bbff5d
suresh-venkate <76463566+suresh-venkate@users.noreply.github.com>
2021-08-11 11:49:23 +0530

    utils.py updated

--- Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/transformer_utils.py
+++ Deep_Learning/Natural_Language_Processing/Transformer_Models/Transformer_From_Scratch/transformer_utils.py
@@ -303,31 +303,6 @@ class Decoder(nn.Module):
       x = layer(x, memory, src_mask, tgt_mask)
     return self.norm(x)
 
-### Class: EncoderDecoder
-class EncoderDecoder(nn.Module):
-    """
-    A standard Encoder-Decoder architecture. Base for this and many 
-    other models.
-    """
-    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
-        super(EncoderDecoder, self).__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-        self.src_embed = src_embed
-        self.tgt_embed = tgt_embed
-        self.generator = generator
-        
-    def forward(self, src, tgt, src_mask, tgt_mask):
-        "Take in and process masked src and target sequences."
-        return self.decode(self.encode(src, src_mask), src_mask,
-                            tgt, tgt_mask)
-    
-    def encode(self, src, src_mask):
-        return self.encoder(self.src_embed(src), src_mask)
-    
-    def decode(self, memory, src_mask, tgt, tgt_mask):
-        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
-
 ### Class: Generator        
 class Generator(nn.Module):
   """
@@ -347,6 +322,37 @@ class Generator(nn.Module):
     x = self.proj(x)
     x = self.smax(x)
     return x
+
+### Class: EncoderDecoder
+class EncoderDecoder(nn.Module):
+  """
+  Overall Encoder + Decoder.
+  """
+  def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+    """
+    Arguments:
+        encoder: Encoder instance
+        decoder: Decoder instance
+        src_embed: Source embeddings (along with positional encoding) to pass to encoder 
+        tgt_embed: Target embeddings (along with positional encoding) to pass to decoder
+        generator: Output generator instance (Linear + Softmax)
+    """
+    super(EncoderDecoder, self).__init__()
+    self.encoder = encoder
+    self.decoder = decoder
+    self.src_embed = src_embed
+    self.tgt_embed = tgt_embed
+    self.generator = generator
+    
+  def encode(self, src, src_mask):
+    return self.encoder(self.src_embed(src), src_mask)
+      
+  def decode(self, memory, src_mask, tgt, tgt_mask):
+    return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
+      
+  def forward(self, src, tgt, src_mask, tgt_mask):
+    "Take in and process masked src and target sequences."
+    return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
     
 ### Function: make_model
 def make_model(src_vocab, tgt_vocab, N=6, 
